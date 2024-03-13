c896c1733c6ef2456f1cc8508db1859c78570cae
Edmario Oliveira <barrigacraft@hotmail.com>
2024-03-13 14:35:29 -0300

    color profile and menu bottom

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,6 +3,12 @@ import { ref, defineComponent } from "vue";
 
 const drawer = ref(false);
 const username = ref(localStorage.getItem("name"));
+const sheetMenuProfile = ref(false);
+const firstWord = ref(username.value.split("")[0]);
+// um array de cores 
+const randomColor = ref(
+  "#" + [0, 1, 2, 3, 4, 5].map(() => Math.floor(Math.random() * 16).toString(16)).join("")
+);
 
 const logout = () => {
   localStorage.clear();
@@ -17,17 +23,22 @@ const logout = () => {
           ><i class="bi bi-list" style="font-size: 1.6rem"></i
         ></v-app-bar-nav-icon>
       </template>
-      <img src="@/assets/img/profile.png" width="60" alt="" class="profile-img" />
+      <v-avatar :color="randomColor" class="mr-2" @click="sheetMenuProfile = true">
+        <!-- <img
+          @click="sheetMenuProfile = true"
+          src="@/assets/img/profile.png"
+          width="60"
+          alt=""
+          class="profile-img"
+        /> -->
+        <span  class="text-h5" style="cursor: pointer;">{{ firstWord }}</span>
+      </v-avatar>
     </v-app-bar>
   </v-layout>
 
   <v-layout>
     <v-navigation-drawer v-model="drawer" temporary width="400">
       <div class="d-flex justify-space-between align-center">
-        <!-- <v-list-item
-          prepend-avatar="https://randomuser.me/api/portraits/lego/1.jpg"
-          :title="username"
-        ></v-list-item> -->
         <div>
           <img src="@/assets/img/logo.png" width="80" alt="" />
         </div>
@@ -53,7 +64,7 @@ const logout = () => {
         ></v-list-item>
 
         <v-divider class="mb-4 mt-4"></v-divider>
-        
+
         <v-list-item
           color="blue-darken-3"
           @click="logout()"
@@ -64,17 +75,53 @@ const logout = () => {
         ></v-list-item>
       </v-list>
     </v-navigation-drawer>
+
+    <v-row justify="center">
+      <v-bottom-sheet v-model="sheetMenuProfile">
+        <v-card height="600">
+          <v-card-title>
+            <h4>{{ username }}</h4>
+          </v-card-title>
+          <v-card-text>
+            <div>
+              <div class="d-flex justify-center align-center">
+                <v-list density="compact" nav width="100%">
+                  
+                  <v-list-item
+                    color="blue-darken-3"
+                    :to="'/minhas-informacoes'"
+                    prepend-icon="mdi-account-circle-outline"
+                    title="Minhas Informações"
+                  ></v-list-item>
+
+                  <v-list-item
+                    color="blue-darken-3"
+                    :to="'/seguranca'"
+                    prepend-icon="mdi-lock"
+                    title="Segurança"
+                  ></v-list-item>
+
+                  <v-divider class="mb-4 mt-4"></v-divider>
+                </v-list>
+              </div>
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue-darken-1"
+              variant="text"
+              @click="sheetMenuProfile = false"
+            >
+              Fechar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-bottom-sheet>
+    </v-row>
   </v-layout>
 </template>
 
 <style scoped>
-.profile-img {
-  border-color: 10px solid rgb(250, 0, 0) !important;
-  padding: 4px;
-  border-radius: 50%;
-  margin-left: 10px;
-  margin-right: 10px;
-  margin-top: 5px;
-  margin-bottom: 5px;
-}
+
 </style>
